f058ce3184f1644dada677882207d6226a5e4413
baichuanjiu <baichuanjiu@gmail.com>
2023-06-02 18:37:33 +0800

    feat(Auth.RPC): 实现了检验用户JWT是否有效的RPC服务

---
+++ Auth.RPC/Services/Authentication/AuthenticationService.cs
@@ -0,0 +1,39 @@
+﻿using Auth.RPC.UserJWT;
+using Grpc.Core;
+using Microsoft.Extensions.Caching.Distributed;
+
+namespace Auth.RPC.Services.Authentication
+{
+    public class AuthenticationService : UserJWT.Auth.AuthBase
+    {
+        //依赖注入
+        private readonly ILogger<AuthenticationService> _logger;
+        private readonly IDistributedCache _distributedCache;
+
+        public AuthenticationService(ILogger<AuthenticationService> logger, IDistributedCache distributedCache)
+        {
+            _logger = logger;
+            _distributedCache = distributedCache;
+        }
+
+        public override Task<AuthReply> Auth(AuthRequest request, ServerCallContext context)
+        {
+            //验证JWT
+            string? currentJWT = _distributedCache.GetString(request.UUID.ToString());
+            if (currentJWT == request.JWT)
+            {
+                return Task.FromResult(new AuthReply
+                {
+                    IsValid = true
+                });
+            }
+            else
+            {
+                return Task.FromResult(new AuthReply
+                {
+                    IsValid = false
+                });
+            }
+        }
+    }
+}
